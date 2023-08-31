9b2083990e3d3d0e2fd07a19b62004b5e427a8d4
Maja Panoska <panoskamaja@live.com>
2023-08-31 16:08:31 +0200

    heroku deployment changes

--- storeproject/settings.py
+++ storeproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_1o^m93tmf-v^++*(&m5hpdh8m#ks20od7er)%73n*vowhw0eu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'bagstore.herokuapp.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- storeproject/urls.py
+++ storeproject/urls.py
@@ -20,7 +20,11 @@ import storeapp.urls as links
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(links))
+    path('', include(links)),
+    path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
