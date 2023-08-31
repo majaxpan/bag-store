007e2f2e185169d4ae2a6a4e3021f18749c296ac
Maja Panoska <panoskamaja@live.com>
2023-08-31 16:18:44 +0200

    modify url version

--- storeproject/urls.py
+++ storeproject/urls.py
@@ -21,10 +21,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(links)),
-    path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
