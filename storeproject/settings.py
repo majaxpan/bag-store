0e9fa421a4445fe2da5833388ab41b5f56823467
Maja Panoska <panoskamaja@live.com>
2023-08-31 16:38:57 +0200

    static root

--- storeproject/settings.py
+++ storeproject/settings.py
@@ -123,6 +123,7 @@ STATICFILES_ROOT = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
