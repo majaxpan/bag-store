e6e5458f2974fce6bbca7fc784d4badc5cec5d7d
Maja Panoska <panoskamaja@live.com>
2023-08-31 16:33:56 +0200

    disable urls

--- storeproject/urls.py
+++ storeproject/urls.py
@@ -19,13 +19,13 @@ from django.urls import path, include
 import storeapp.urls as links
 from django.conf import settings
 from django.conf.urls.static import static
-
-from django.views.static import serve
-from django.urls import re_path
+#
+# from django.views.static import serve
+# from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(links)),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    # re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    # re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
